db8d50185e0c2e44aa247ed126ab9d1fe45225c2
Jeram Calbe <calbejeram@gmail.com>
2023-07-31 11:06:22 +0800

    Add backtotop component in home section component

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
 import HomeSection from '../components/HomeSection';
+import BackToTop from '../components/BackToTop'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Hero/>
             <HomeSection/>
             <Footer/>
+            <BackToTop/>
         </Stack>
     </Box>
   );
